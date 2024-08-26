c6562da798d6c638e9a7cf4a5290f1f4ff5a9b35
nsay <narithsay@gmail.com>
2024-08-25 23:21:55 -0400

    added ngx-bootstrap, bootstrap, font-awesome

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import {Component, inject, OnInit} from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
