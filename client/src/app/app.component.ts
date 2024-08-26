6df8f36219b0cb1d1e3b64af64cca063c6602145
nsay <narithsay@gmail.com>
2024-08-25 21:51:05 -0400

    added CORS

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  http = inject(HttpClient);
   title = 'client';
+  users: any;
+
+  ngOnInit() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => { this.users = response },
+      error: error => { console.log(error) },
+      complete: () => console.log('Completed!'),
+    })
+  }
 }
