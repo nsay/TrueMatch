416ec905fbbff5198f9cb8a5befec2ba6bda75eb
nsay <narithsay@gmail.com>
2025-09-25 17:02:44 -0400

    adding JWT token auth

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,40 @@
+﻿using API.Entities;
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API.Services
+{
+    public class TokenService(IConfiguration config) : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            var tokenKey = config["TokenKey"];
+            if (string.IsNullOrEmpty(tokenKey) || tokenKey.Length < 64)
+                throw new Exception("TokenKey must be at least 64 characters long");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+
+            var claims = new List<Claim>
+            {
+                new (ClaimTypes.Email, user.Email),
+                new (ClaimTypes.NameIdentifier, user.Id),
+            };
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
